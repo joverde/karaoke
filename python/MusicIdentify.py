3983a1c135c68a5fde1d01a44e2866d16f2d5d38
Josh <jover@mit.edu>
2019-09-14 16:44:47 -0400

    Organization, first requests, etc.

--- python/MusicIdentify.py
+++ python/MusicIdentify.py
@@ -5,13 +5,12 @@ import requests
 
 
 def hello_world(request):
-	artist = request["Artist Name"].value
-	song = request["Song Name"].value
+	artist = form["Artist Name"].value
+	song = form["Song Name"].value
 	print("<p>Now Displaying content for:", artist, '-', song)
 	data = {'url': 'https://audd.tech/'+artist + ' '+ song,'return': 'timecode,apple_music,deezer,spotify','api_token': 'd54c12502bf9f8c2fb1af38c26b3f58b'}
 	result = requests.post('https://api.audd.io/', data=data)
-	#"<p>",result.text,"<p>"
-	return Response()
+	return Response("<p>",result.text,"<p>")
 
 if __name__ == '__main__':
     with Configurator() as config:
