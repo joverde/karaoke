f06b6861cd2ba3bf494d83c1d122f4dbf1ef862c
Josh <jover@mit.edu>
2019-09-14 15:09:39 -0400

    Organization, first requests, etc.

--- MusicIdentify.py
+++ MusicIdentify.py
@@ -11,6 +11,3 @@ data = {
 result = requests.post('https://api.audd.io/', data=data)
 print(result.text)
 
-index = open("index.html").read().format(first_header=result.text, 
-                                         p1='World', 
-                                         p2=result.text)
