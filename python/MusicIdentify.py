31baba6b182c4ccf4c86c06e202586e714fee907
Josh <jover@mit.edu>
2019-09-14 15:48:35 -0400

    Organization, first requests, etc.

--- python/MusicIdentify.py
+++ python/MusicIdentify.py
@@ -1,8 +1,20 @@
 import requests
+import cgi
+import cgitb
+
+form = cgi.FieldStorage()
+if "Artist Name" not in form or "Song Name" not in form:
+    print("<H1>Error</H1>")
+    print("Please fill in the name and addr fields.")
+    return
+artist = form["Artist Name"].value
+song = form["Song Name"].value
+print("<p>Now Displaying content for:", artist, '-', song)
+
 data = {
-    'url': 'https://audd.tech/example1.mp3',
+    'url': 'https://audd.tech/'+artist + ' '+ song,
     'return': 'timecode,apple_music,deezer,spotify',
-    'api_token': 'test'
+    'api_token': 'd54c12502bf9f8c2fb1af38c26b3f58b'
 }
 result = requests.post('https://api.audd.io/', data=data)
-print(result.text)
+print("<p>",result.text,"<p>")
