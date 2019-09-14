a6df277287e3c5038748eb3056fc4344b7b8d93b
Josh <jover@mit.edu>
2019-09-14 15:01:54 -0400

    Organization, first requests, etc.

---
+++ MusicIdentify.py
@@ -0,0 +1,16 @@
+import requests
+import cgi
+form = cgi.FieldStorage()
+artist =  form.getvalue('Artist Name')
+song =  form.getvalue('Song Name')
+data = {
+    'url': 'https://audd.tech/'+artist + ' '+ song,
+    'return': 'timecode,apple_music,deezer,spotify',
+    'api_token': 'd54c12502bf9f8c2fb1af38c26b3f58b'
+}
+result = requests.post('https://api.audd.io/', data=data)
+print(result.text)
+
+index = open("index.html").read().format(first_header='result.text', 
+                                         p1='World', 
+                                         p2='result.text')
